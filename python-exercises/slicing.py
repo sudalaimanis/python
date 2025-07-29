e1338647689077b481629ba6c7a1129371b20475
sudalaimani <93456174+sudalaimanis@users.noreply.github.com>
2025-07-29 22:10:49 +0000

    add additional examples for string and tuple slicing

--- python-exercises/slicing.py
+++ python-exercises/slicing.py
@@ -11,5 +11,18 @@ print(text[-2])
 print(text[-3])
 
 # slicing in strings
+print("================")
+print("slicing in strings")
 print(text[0:4])
 print(text[:]) # print the whole string
+print(text[:7])
+print(text[11:])
+
+# slicing in tuple
+print("================")
+print("slicing in tuple")
+devops = ("dev", "ops", "python", "java")
+print(devops[0:2])
+print(devops[:])  # print the whole tuple
+print(devops[2:3][0])  # accessing the first element of the sliced tuple
+print(devops[2:5][0][2:6])  # accessing a slice of the first element of the sliced tuple
